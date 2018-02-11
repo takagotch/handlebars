fd183c4bf28402a2b16fdc9839748ab51be3c89a
takagotch <dyaccb@gmail.com>
2018-02-12 07:09:42 +0900

    return self;

---
+++ fr/cmd.sh
@@ -0,0 +1,5 @@
+npm install handlebars -g
+
+handlebars <input>-f </input>
+handlebars library.handlebars -f templates.js
+
---
+++ fr/template.js
@@ -0,0 +1,12 @@
+function(){
+var template = Handlebars.template,
+	templates = Handlebars.templates = Handlebars.templates || {};
+templates['library'] =
+	template(function(Handlebars, depth0, helpers, partials, data){
+
+		template: Handlebars.templates['library'],
+
+	});
+}
+
+
